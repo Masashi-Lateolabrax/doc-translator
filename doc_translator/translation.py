71e602e27fe84ad4692cbfd5c6756b1205fe38a2
masashi <masashi.lateolabrax@gmail.com>
2024-09-16 21:14:29 +0900

    add: Translator class

---
+++ doc_translator/translation.py
@@ -0,0 +1,43 @@
+import os
+
+from . import interface as inf
+
+
+class Translator:
+    def __new__(cls, *args, **kwargs):
+        raise RuntimeError("Use the static methods. to instantiate this class.")
+
+    def __init__(self, translator: inf.Translator, **settings):
+        self._translator = translator
+
+    @staticmethod
+    def chatgpt(**settings):
+        from .translator import ChatGPT
+
+        translator = ChatGPT(**settings)
+
+        this: Translator = object.__new__(Translator)
+        this.__init__(translator, **settings)
+
+        return this
+
+    def translate(self, src_path, dst_path):
+        _, ext = os.path.splitext(src_path)
+
+        if ".docx" == ext:
+            from .formatter import DocxFormatter
+            fm = DocxFormatter(src_path=src_path)
+        else:
+            raise RuntimeError("Not support")
+
+        while True:
+            chunk = fm.get_chunk()
+            if chunk is None:
+                break
+            text = chunk.read()
+            translated = self._translator.translate_line(text)
+            print(text)
+            print(translated)
+            chunk.write(translated)
+
+        fm.save(dst_path)
